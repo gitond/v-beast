1c50288c8b7d4fd74cf44da3106fc9609014aee3
Botond Ortutay <boortu@utu.fi>
2024-01-18 14:10:36 +0200

    set up a container and a grid - resolves #3

---
+++ src/components/gameContainer.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import gameScene from './gameScene.vue';
+</script>
+
+<template>
+    <div class="gamecontainer">
+        <gameScene />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/gameScene.vue
@@ -0,0 +1,34 @@
+<script setup>
+    /*
+        the game is played in a 25y40x grid
+    */
+    let gamestate = [];
+    // filling the gamestate with empty tiles
+    for (let i = 0; i<25; i++){
+        gamestate.push([]);
+        for (let j = 0; j<40; j++){
+            gamestate[i].push("e");
+        }
+    }
+
+    // test & debug area
+    // --- //
+    //gamestate[13][19] = "p";
+    //console.log(gamestate);
+    //<span v-for="n in 1000">{{ gamestate[n] }}</span>
+</script>
+
+<template>
+    <div class="gamescene">
+        <table>
+            <caption>Placeholder</caption>
+            <tbody>
+                <tr v-for="row in gamestate">
+                    <td v-for="j in 40">{{ row[j] }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped></style>
